4f16a439610f4191084143e851f70fa82d5b3a73
roy-eugene049 <roy.eugenes000.com>
2023-05-29 13:47:48 +0300

    Initial Commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Software Engineer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Web Developer",
       icon: creator,
     },
   ];
